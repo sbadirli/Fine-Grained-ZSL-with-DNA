3c54bbeeb33ecd5d85dfa43e3ed950c0b9453264
Sarkhan Badirli <s.badirli@gmail.com>
2021-10-22 16:43:29 -0400

    Update load_tuned_params.m

--- BZSL/load_tuned_params.m
+++ BZSL/load_tuned_params.m
@@ -13,15 +13,15 @@ function [att, K, k_0, k_1, m, s, a_0 , b_0] = load_tuned_params(fname, dataset,
     dim  = 500;
     
     INSECT  = [0.1, 10, 5*dim, 10, 3];
-    if strcmp(dataset, 'CUB_DNA')
-        if strcmp(side_info, 'visual')
-            CUB_DNA  = [1, 25, 500*dim, 10, 3];
+    if strcmp(dataset, 'CUB')
+        if strcmp(side_info, 'original')
+            CUB  = [1, 25, 500*dim, 10, 3];
         elseif strcmp(side_info, 'w2v')
-            CUB_DNA  = [0.1, 25, 5*dim, 5, 2];
+            CUB  = [0.1, 25, 5*dim, 5, 2];
             att = att_w2v;
         else
-            CUB_DNA  = [0.1, 25, 25*dim, 5, 3];
-            att = att_exp;
+            CUB  = [0.1, 25, 25*dim, 5, 3];
+            att = att_dna;
         end
     end
     
